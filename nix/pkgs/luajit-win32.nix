8b7516c55d1c62d4a236bb68e6f5568e89f6b1f7
bin <bin@sleepy.rs>
2024-08-12 19:23:03 -0400

    LuaJIT: switch to 2.0.2

--- nix/pkgs/luajit-win32.nix
+++ nix/pkgs/luajit-win32.nix
@@ -7,13 +7,13 @@
 
 stdenv.mkDerivation rec {
   pname = "LuaJIT";
-  version = "2.0.0";
+  version = "2.0.2";
 
   src = fetchFromGitHub {
     owner = pname;
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-j80P5Fv0q9WuH59uu4CbMOg8/Y6KCqwVnaz3rggMUpk=";
+    hash = "sha256-iN7g0UOmTWZjamxpRVC2S+f6PgPbbGGQEzuKaD+rjx8=";
   };
 
   makeFlags = [
